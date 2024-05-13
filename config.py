2c16595e9b6a3f1f9dd97e3f7c5381307ace5788
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-05-13 23:25:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOGGER_ID = int(getenv("LOGGER_ID", "-1002077359484"))
 
 # Get this value from @AnjaliMusicFourBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "6350757877"))
+OWNER_ID = int(getenv("OWNER_ID", "6260080241"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -42,8 +42,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "http://t.me/+YvPfBPHypFgzOTY9")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/RBGOFFICIAL1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "http://t.me/AnjalixSupportxGroup")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AnjalixSupportxGroup")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", "false"))
