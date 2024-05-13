9369ee9d92020f875471466349ce13205e03aad8
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-05-13 23:21:15 +0530

    Update userbot.py

--- VenomX/core/userbot.py
+++ VenomX/core/userbot.py
@@ -54,8 +54,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("RBGOFFICIAL1")
-                await self.one.join_chat("NEON_SUKOON")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(1)
@@ -83,8 +83,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.one.join_chat("RBGOFFICIAL1")
-                await self.one.join_chat("NEON_SUKOON")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(2)
@@ -112,8 +112,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.one.join_chat("RBGOFFICIAL1")
-                await self.one.join_chat("NEON_SUKOON")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.one.join_chat("RBGOFFICIAL1")
-                await self.one.join_chat("NEON_SUKOON")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(4)
@@ -170,8 +170,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.one.join_chat("RBGOFFICIAL1")
-                await self.one.join_chat("NEON_SUKOON")
+                await self.one.join_chat("AnjalixSupportxGroup")
+                await self.one.join_chat("AnjalixSupportxGroup")
             except:
                 pass
             assistants.append(5)
